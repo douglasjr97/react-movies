ee9de661163c1c9e3e6af35f2ce924d5c291ae3d
Douglas Falcao <falcao.douglas97@gmail.com>
2022-06-16 03:50:24 -0300

    refactor: components

--- src/screens/Details/index.tsx
+++ src/screens/Details/index.tsx
@@ -1,17 +1,25 @@
-import { View, Text } from "react-native";
+import { View, Text, FlatList } from "react-native";
 import React from "react";
 import { useContext } from "react";
 import {
   FilmesContext,
   useFilmesContext,
 } from "../../contexts/FilmesContext/FilmesContext";
+import Filmes from "../../components/Filmes";
+import { Movie } from "../../interfaces/interface";
 
 export default function Details() {
-  const { filme } = useContext(FilmesContext);
+  const { filme, setFilme } = useContext(FilmesContext);
   console.log(filme);
   return (
     <View>
       <Text>Details</Text>
+
+      {/* <FlatList 
+        data={filme as Movie}
+        keyExtractor={(item) => String(item.id)}
+        renderItem={({item}) => <Filmes data={item}/>}
+      /> */}
     </View>
   );
 }
